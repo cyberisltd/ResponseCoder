e7871c3cd5917be5f205793a3551dbf37ded4d89
cyberisltd <github@cyberis.co.uk>
2013-06-27 12:54:19 +0100

    Added a low[er] level Perl script to allow easy manipulation of HTTP version headers (e.g. HTTP/10.2)

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ ResponseCoder
 * Copyright: Cyberis Limited 2013
 * License: GPLv3 (See LICENSE)
 
-A server-side PHP script to manipulate HTTP Response Headers, designed to identify weaknesses in perimeter filtering devices (e.g. web proxies and next generation firewalls)
+A server-side PHP script to manipulate HTTP Response Headers, designed to identify weaknesses in perimeter filtering devices (e.g. web proxies and next generation firewalls). Also includes a low[er] level Perl script allowing you to manipulate the entire response (such as HTTP version etc) - httpversioncoder.pl.
 
 Description
 -----------
@@ -18,11 +18,20 @@ Obviously manipulation of such HTTP response headers may lead to unexpected resu
 
 There are numerous tests you can conduct with HTTP response headers (take a look over at <a href="http://greenbytes.de/tech/tc2231/">http://greenbytes.de/tech/tc2231/</a> for some ideas), and this script certainly doesn't expose all possible scenarios. However it does provide a quick testing framework which is easier to use and more intuitive than NetCat.
 
+An online version of the PHP script is accessible in our labs - http://labs.cyberis.co.uk/ResponseCoder/
+
 Dependencies
 ------------
 Apache and PHP are required (other web servers that support PHP may work - but you'll need to check how the server handles tampering with response headers). It is recommended that you edit php.ini to remove any default 'Content-Type' header - set *default_mimetype* to null.
 
 You'll also need to ensure you have a Unix compress tool (e.g ncompress) installed for 'compress' Content-Type encodings.
+
+If you want to dive even deeper, you'll need Perl and the following modules to use httpversioncoder.pl:
+
+```perl
+use Term::ANSIColor;
+use IO::Socket;
+```
 
 Issues
 ------
---
+++ httpversioncoder.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl
+
+# Basic web server responding with a small page and logging the request to STDOUT
+# Returns an incorrect HTTP version number to see how intermediary devices handle the request
+
+# Copyright(C) 2013 Cyberis Limited
+# Author: geoff.jones@cyberis.co.uk
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+use Term::ANSIColor;
+use IO::Socket;
+
+
+# Create socket (needs root of course)
+my $sock = new IO::Socket::INET (
+	LocalPort => '81',
+	Proto => 'tcp',
+	Listen => 10,
+	ReuseAddr => 1,
+) || die "Could not create socket: $!\n";
+
+# Prepate the response
+my @httpresponse = <DATA>;
+my $file = "./helloworld.exe";
+open (FILE, $file) or die "[ERROR] Cannot open file $file";
+push (@httpresponse, <FILE>);
+
+# Start listening
+print &httpLabel . "Listening...\n\n";
+
+my $client;
+while ($client = $sock->accept()) {
+
+	next if my $pid = fork;
+	die "Error forking child process - $!\n" unless defined $pid;
+
+	my @httprequest;
+
+	my $line = <$client>;
+
+	while (defined ($line) && $line ne "\r\n") {
+           	push (@httprequest,$line);
+		$line = <$client>;
+	}
+	
+	# Print the request
+	print &httpLabel;
+	foreach (@httprequest) {
+		print $_;
+	}
+	
+	# Print the response
+	foreach (@httpresponse) {
+		print $client $_;
+	}
+	print "\n";
+
+	# Clost the client connection
+	$client->close;
+	exit( fork );
+}
+continue
+{
+	close($client);
+	kill CHLD => -$$;
+}
+
+sub httpLabel {
+	print color 'green';
+	print "[HTTP]\n";
+	print color 'reset';
+	return "";
+}
+
+__DATA__
+HTTP/3.2 200 OK
+Server: Fake-dns-web
+Content-Type: appliation/octet-stream
+Content-Disposition: attachment; filename="helloworld.exe";
+
